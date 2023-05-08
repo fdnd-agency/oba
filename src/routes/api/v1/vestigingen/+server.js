8e637c5a9ce772fb38a6b8339826b3baf0f13d52
Krijn Hoetmer <krijn@qontent.nl>
2023-05-08 12:20:52 +0200

    Add ?id to /vestigingen endpoint

--- src/routes/api/v1/vestigingen/+server.js
+++ src/routes/api/v1/vestigingen/+server.js
@@ -7,32 +7,63 @@ export async function GET({ url }) {
   let skip = Number(url.searchParams.get('skip') ?? 0)
   let direction = url.searchParams.get('direction') === 'ASC' ? 'ASC' : 'DESC'
   let orderBy = (url.searchParams.get('orderBy') ?? 'publishedAt') + '_' + direction
-
-  const query = gql`
-    query getVestigingen($first: Int, $skip: Int, $orderBy: VestigingOrderByInput) {
-      vestigingen(first: $first, skip: $skip, orderBy: $orderBy) {
-        id
-        naam
-        adres
-        foto {
-          height
-          width
-          original: url
-          small: url(transformation: { image: { resize: { width: 500, fit: clip } } })
-          originalAsWebP: url(transformation: { document: { output: { format: webp } } })
-          smallAsWebP: url(
-            transformation: { image: { resize: { width: 500, fit: clip } }, document: { output: { format: webp } } }
-          )
+  let id = url.searchParams.get('id') ?? false
+  let query
+  let data
+  
+  if (id) {
+    query = gql`
+      query getVestigingen($id: ID) {
+        vestigingen(where: {id: $id}) {
+          id
+          naam
+          adres
+          foto {
+            height
+            width
+            original: url
+            small: url(transformation: { image: { resize: { width: 500, fit: clip } } })
+            originalAsWebP: url(transformation: { document: { output: { format: webp } } })
+            smallAsWebP: url(
+              transformation: { image: { resize: { width: 500, fit: clip } }, document: { output: { format: webp } } }
+            )
+          }
+          geolocatie {
+            latitude
+            longitude
+          }
+          studieplekken
         }
-        geolocatie {
-          latitude
-          longitude
-        }
-        studieplekken
       }
-    }
-  `
-
-  const data = await hygraph.request(query, { first, skip, orderBy })
+    `
+    data = await hygraph.request(query, { id })
+  } else {
+    
+    query = gql`
+      query getVestigingen($first: Int, $skip: Int, $orderBy: VestigingOrderByInput) {
+        vestigingen(first: $first, skip: $skip, orderBy: $orderBy) {
+          id
+          naam
+          adres
+          foto {
+            height
+            width
+            original: url
+            small: url(transformation: { image: { resize: { width: 500, fit: clip } } })
+            originalAsWebP: url(transformation: { document: { output: { format: webp } } })
+            smallAsWebP: url(
+              transformation: { image: { resize: { width: 500, fit: clip } }, document: { output: { format: webp } } }
+            )
+          }
+          geolocatie {
+            latitude
+            longitude
+          }
+          studieplekken
+        }
+      }
+    `
+    data = await hygraph.request(query, { first, skip, orderBy })
+  }
   return new Response(JSON.stringify(data), responseInit)
 }
--- src/routes/docs/vestigingen/+page.svelte
+++ src/routes/docs/vestigingen/+page.svelte
@@ -2,7 +2,8 @@
 
 <h3 id="get">GET</h3>
 
-<ul><li>
-  <a href="/api/v1/vestigingen">/api/v1/vestigingen</a>
-  <a href="/api/v1/vestigingen?first=100">/api/v1/vestigingen?first=100</a>
-</li></ul>
+<ul>
+  <li><a href="/api/v1/vestigingen">/api/v1/vestigingen</a></li>
+  <li><a href="/api/v1/vestigingen?first=100">/api/v1/vestigingen?first=100</a></li>
+  <li><a href="/api/v1/vestigingen?id=clgnow0uifttp0buv22i3hc0h">/api/v1/vestigingen?id=clgnow0uifttp0buv22i3hc0h</a></li>
+</ul>
