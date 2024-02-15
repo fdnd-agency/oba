331acfd0a19ffdf1d6a57ae090cafc0d8cd73375
David van Boxtel <davidvb00@gmail.com>
2024-02-15 13:53:08 +0100

    update carousel homepagina

---
+++ src/lib/molecules/carouselRecenteLeeslijst.svelte
@@ -0,0 +1,24 @@
+<script>
+
+
+
+</script>
+
+<!-- <section>
+	<article>
+		<h2>Jouw recente leeslijst: <span>boeken<span></span></span></h2>
+		<p>{homepage[1].homepageText}</p>
+		<LinkButton buttonText="Bekijk Leeslijst" buttonLink="/leeslijst" />
+	</article>
+
+	<article class="carousel-container carousel">
+		{#each data.apiBooks.results.slice(0, 5) as book}
+			<Card
+				bookAuthor={book.authors}
+				bookTitle={book.titles}
+				bookUrl={book.coverimages}
+				bookDetailLink={book.detailLink}
+			/>
+		{/each}
+	</article>
+</section> -->
