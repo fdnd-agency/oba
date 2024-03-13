9bbcd3aeefb066627d9f32bb56dfa406396aaff7
joostf <j.faber@hva.nl>
2024-03-13 15:18:20 +0100

    Revert "why not?"
    This reverts commit a09b88875b268fb5c56d5d4c52b4f1e4424483c3.

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,35 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8'),
+	() => import('./nodes/9')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [~2],
+		"/betalingen": [~4],
+		"/leeslijst": [~5],
+		"/login": [6],
+		"/reserveringen": [~7],
+		"/signup": [8],
+		"/uitleningen": [~9],
+		"/[zoekresultaten]": [~3]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+
+	reroute: (() => {})
+};
+
+export { default as root } from '../root.svelte';
